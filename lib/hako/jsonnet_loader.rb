12da8785283f6013f5e403388fc47e85f92249b5
Kohei Suzuki <eagletmt@gmail.com>
2017-11-02 18:14:07 +0900

    Load all available env providers

--- lib/hako/jsonnet_loader.rb
+++ lib/hako/jsonnet_loader.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-require 'jsonnet'
-require 'json'
+require 'hako'
 require 'hako/env_providers'
 require 'hako/loader'
+require 'json'
+require 'jsonnet'
 
 module Hako
   class JsonnetLoader
@@ -22,14 +23,16 @@ module Hako
     private
 
     def define_provider_functions
-      # TODO: List all available env providers
-      %w[
-        file
-        yaml
-      ].each do |provider_name|
-        provider_class = Loader.new(Hako::EnvProviders, 'hako/env_providers').load(provider_name)
-        @vm.define_function("provide.#{provider_name}") do |options, name|
-          provider_class.new(@root_path, JSON.parse(options)).ask([name]).fetch(name)
+      Gem.loaded_specs.each do |gem_name, spec|
+        spec.require_paths.each do |path|
+          Dir.glob(File.join(spec.full_gem_path, path, 'hako/env_providers/*.rb')).each do |provider_path|
+            provider_name = File.basename(provider_path, '.rb')
+            provider_class = Loader.new(Hako::EnvProviders, 'hako/env_providers').load(provider_name)
+            Hako.logger.debug("Loaded #{provider_class} from '#{gem_name}' gem")
+            @vm.define_function("provide.#{provider_name}") do |options, name|
+              provider_class.new(@root_path, JSON.parse(options)).ask([name]).fetch(name)
+            end
+          end
         end
       end
     end
